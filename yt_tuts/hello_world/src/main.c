ef0b6fc6887ea24b5f0032fe0f97d711f3cf5543
vishal-ahirwar <vishlchoudhry0091@gmail.com>
2022-04-27 23:59:06 +0530

    empty commit

---
+++ yt_tuts/hello_world/src/main.c
@@ -0,0 +1,9 @@
+#include"../include/hello_world.h"
+void hello_world()
+{
+    printf("hello world!");
+};
+void println(const char*str)
+{
+    printf("\nmessage : %s\n",str);
+};
---
+++ yt_tuts/src/main.c
@@ -0,0 +1,8 @@
+#include"../hello_world/include/hello_world.h"
+
+int main()
+{
+    hello_world();
+    println("yeah! this is println() from C\n");
+    return 0;
+};
